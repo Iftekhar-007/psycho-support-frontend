80b63379f6feeac123fff11c80e4777989443561
Iftekhar-007 <shawonsp534@gmail.com>
2026-08-19 22:52:53 +0600

    okay

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,19 +1,51 @@
+// import { inferAdditionalFields } from "better-auth/client/plugins";
+// import { createAuthClient } from "better-auth/react";
+
+// export const authClient = createAuthClient({
+//   baseURL:
+//     process.env.NEXT_PUBLIC_BACKEND_API_URL ??
+//     "https://psychology-support-backend.vercel.app",
+//   fetchOptions: {
+//     credentials: "include",
+//   },
+//   plugins: [
+//     inferAdditionalFields({
+//       user: {
+//         role: { type: "string" },
+//         isDeleted: { type: "boolean", input: false },
+//         deletedAt: { type: "date", required: false, input: false },
+//       },
+//     }),
+//   ],
+// });
+
+// export const { signIn, signUp, signOut, useSession } = authClient;
+
 import { inferAdditionalFields } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL:
-    process.env.NEXT_PUBLIC_BACKEND_API_URL ??
-    "https://psychology-support-backend.vercel.app",
+  baseURL: "https://psychology-support-backend.vercel.app",
+
   fetchOptions: {
     credentials: "include",
   },
+
   plugins: [
     inferAdditionalFields({
       user: {
-        role: { type: "string" },
-        isDeleted: { type: "boolean", input: false },
-        deletedAt: { type: "date", required: false, input: false },
+        role: {
+          type: "string",
+        },
+        isDeleted: {
+          type: "boolean",
+          input: false,
+        },
+        deletedAt: {
+          type: "date",
+          required: false,
+          input: false,
+        },
       },
     }),
   ],
